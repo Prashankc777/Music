9036a9d28c16db8742e2893aaab733ba159faa18
prashan kc <prashankc777@gmail.com>
2022-07-25 20:39:12 +0545

    minor change

---
+++ Music APi/Controllers/SongsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Music_APi.Models;
+
+namespace Music_APi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song()
+            {
+                Id = 1,
+                Language = "Nepali",
+                Title = "Romantic"
+            },
+            new Song()
+            {
+                Id = 2,
+                Language = "Nepali",
+                Title = "Romantic"
+
+            }
+
+        };
+
+        [HttpGet, Route("Songs")]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Insert(Song song)
+        {
+            songs.Add(song);
+        }
+    }
+}
